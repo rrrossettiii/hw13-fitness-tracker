9471576165a7153f3ec57340570621a122ce4028
rrrossettiii <rrrossettiii@gmail.com>
2020-11-18 23:38:19 -0500

    moved htmls to /pages and added routes to them in /routes

---
+++ routes/Views.js
@@ -0,0 +1,17 @@
+// Dependencies;
+// =============:
+const router = require("express").Router();
+const path = require("path");
+
+// - Exercise;
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../pages/exercise.html"));
+});
+
+// - Stats;
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../pages/stats.html"));
+});
+
+// - Exports;
+module.exports = router;
--- server.js
+++ server.js
@@ -4,23 +4,30 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+// Express;
+// =============:
 const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
-
 const app = express();
-
+// - data parser;
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// - set static files in /public;
 app.use(express.static("public"));
 
+// Mongoose;
+// =============:
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
   useNewUrlParser: true
 });
 
+// Routes;
+// =============:
+app.use(require("./routes/Views.js"));
+
+
+// Server Listener;
+// =============:
 app.listen(PORT, () => {
   console.log(
     "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
